7fa2fcc87758f4fd380060e691a3ba309ae165cd
dzy <duzhiyuan11111@gmail.com>
2018-03-02 13:48:20 +0800

    固定图钉

---
+++ src/app/nz-demo-affix/nz-demo-affix.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-nz-demo-affix',
+  templateUrl: './nz-demo-affix.component.html',
+  styleUrls: ['./nz-demo-affix.component.css']
+})
+export class NzDemoAffixComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
